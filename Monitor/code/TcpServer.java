313c1cad9881c0a1787e5bb3d1ed40b445250a83
Junjie Kai <opendoublej@gmail.com>
2015-12-02 00:23:50 -0500

    Create TcpServer.java

---
+++ Monitor/code/TcpServer.java
@@ -0,0 +1,41 @@
+package code;
+
+import java.net.*;
+import java.io.*;
+
+public class TcpServer extends Thread {
+
+	private ServerSocket serverSocket;
+
+	public TcpServer(int port) throws IOException {
+		serverSocket = new ServerSocket(port);
+		serverSocket.setSoTimeout(10000);
+	}
+
+	public void run() {
+		while (true) {
+			try {
+				System.out.println("Waiting for client on port "
+						+ serverSocket.getLocalPort() + "...");
+				Socket server = serverSocket.accept();
+				System.out.println("Just connected to "
+						+ server.getRemoteSocketAddress());
+				DataInputStream in = new DataInputStream(
+						server.getInputStream());
+				System.out.println(in.readUTF());
+				DataOutputStream out = new DataOutputStream(
+						server.getOutputStream());
+				out.writeUTF("Thank you for connecting to "
+						+ server.getLocalSocketAddress() + "\nGoodbye!");
+				server.close();
+			} catch (SocketTimeoutException s) {
+				System.out.println("Socket timed out!");
+				break;
+			} catch (IOException e) {
+				e.printStackTrace();
+				break;
+			}
+		}
+	}
+
+}

a6133e50a6ae25109193d50371dafb69e90fb8d4
Brandon To <brandon_to@hotmail.com>
2015-12-04 18:41:15 -0500

    Added TFTP read functionality to firmware.

--- Firmware/main.c
+++ Firmware/main.c
@@ -17,6 +17,11 @@
 //
 //*****************************************************************************
 
+
+//*****************************************************************************
+// INCLUDES
+//*****************************************************************************
+
 // Standard includes
 #include <stdlib.h>
 
@@ -38,7 +43,7 @@
 #include "datatypes.h"
 #include "tftp.h"
 
-//Common includes
+// Common includes
 #include "network_if.h"
 #include "uart_if.h"
 #include "udma_if.h"
@@ -48,18 +53,38 @@
 #include "pinmux.h"
 
 
-// Vector table
+//*****************************************************************************
+// DEFINES
+//*****************************************************************************
+#define TFTP_IP			0xC0A8006D	/* This is the host IP */
+#define FILE_SIZE_MAX	(20*1024)	/* Max File Size set to 20KB */
+#define SSID 			"NETGEAR31"
+#define SSID_KEY		"happystar329"
+
+
+//*****************************************************************************
+// VARIABLES
+//*****************************************************************************
+// Vector table defined exterenally (in startup_css.c)
 extern void (* const g_pfnVectors[])(void);
 
+
+//*****************************************************************************
+// FUNCTION PROTOTYPES
+//*****************************************************************************
 static void BoardInit(void);
 static void TFTPTransfer(void);
 
+
+//*****************************************************************************
+// MAIN
+//*****************************************************************************
 void main() 
 {
     long lRetVal = -1;
 
     // Board Initialization
-	BoardInit();
+    BoardInit();
 
     // Enable and configure DMA
     UDMAInit();
@@ -79,15 +104,19 @@ void main()
     }
 
     // Start TFTP transfer
-    TFTPTransfer()
+    TFTPTransfer();
 
     // Hang at end of execution
     LOOP_FOREVER();
 }
 
+
+//*****************************************************************************
+// FUNCTION IMPLEMENTATIONS
+//*****************************************************************************
 static void BoardInit(void)
 {
-	// Set base of vector table
+    // Set base of vector table
     IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
 
     // Enable Processor
@@ -99,7 +128,93 @@ static void BoardInit(void)
 
 static void TFTPTransfer(void)
 {
-	// TODO (Brandon): Test TFTP Transfer to and from a TFTP server
+    SlSecParams_t secParams;
+    unsigned char *pucFileBuffer = NULL;  // Data read or to be written
+    unsigned long uiFileSize;
+
+    char *FileRead = "readFromServer.txt";	// File to be read using TFTP.
+    char *FileWrite = "writeToServer.txt";	// File to be written using TFTP.
+
+    long pFileHandle;			// Pointer to file handle
+    SlFsFileInfo_t pFsFileInfo;
+    long lRetVal = -1;
+    unsigned short uiTftpErrCode;
+
+    // Configuring security parameters for the AP
+    secParams.Key = NULL;
+    secParams.KeyLen = 0;
+    secParams.Type = SL_SEC_TYPE_OPEN;
+
+    // Initialize network driver
+    lRetVal = Network_IF_InitDriver(ROLE_STA);
+
+    // Connecting to WLAN AP - Set with static parameters defined at the top
+    // After this call we will be connected and have IP address
+    lRetVal = Network_IF_ConnectAP(SSID, secParams);
+
+    // Output IP to terminal
+    UART_PRINT("Connecting to TFTP server %d.%d.%d.%d\n\r",\
+                  SL_IPV4_BYTE(TFTP_IP, 3), SL_IPV4_BYTE(TFTP_IP, 2),
+                  SL_IPV4_BYTE(TFTP_IP, 1), SL_IPV4_BYTE(TFTP_IP, 0));
+
+    // Set limit on file size
+    uiFileSize = FILE_SIZE_MAX;
+
+    // Allocate heap space for file
+    pucFileBuffer = malloc(uiFileSize);
+    if(NULL == pucFileBuffer)
+    {
+        UART_PRINT("Can't Allocate Resources\r\n");
+        LOOP_FOREVER();
+    }
+
+    // Initialize heap space to 0 bytes
+    memset(pucFileBuffer, '\0', uiFileSize);
+
+    // Receive file using TFTP and store in pucFileBuffer
+    lRetVal = sl_TftpRecv(TFTP_IP, FileRead, (char *)pucFileBuffer,\
+                            &uiFileSize, &uiTftpErrCode );
+    if(lRetVal < 0)
+    {
+        free(pucFileBuffer);
+        ERR_PRINT(lRetVal);
+        LOOP_FOREVER();
+    }
+
+    // Populate pFsFileInfo with file information FileRead
+    lRetVal = sl_FsGetInfo((unsigned char *)FileRead, NULL, &pFsFileInfo);
+
+    if(lRetVal < 0 )
+    	// Creates file if sl_FsGetInfo fails (file does not exist)
+        lRetVal = sl_FsOpen((unsigned char *)FileRead,\
+                FS_MODE_OPEN_CREATE(FILE_SIZE_MAX, _FS_FILE_OPEN_FLAG_COMMIT|\
+                  _FS_FILE_PUBLIC_WRITE), NULL, &pFileHandle);
+    else
+    	// Opens file if sl_FsGetInfo succeeds (file exists)
+        lRetVal = sl_FsOpen((unsigned char *)FileRead, FS_MODE_OPEN_WRITE, \
+                            NULL, &pFileHandle);
+
+    if(lRetVal < 0)
+    {
+        free(pucFileBuffer);
+        ERR_PRINT(lRetVal);
+        LOOP_FOREVER();
+    }
+
+    // Write contents of file buffer to file
+    lRetVal = sl_FsWrite(pFileHandle, 0, pucFileBuffer, uiFileSize);
+    if(lRetVal < 0)
+    {
+        free(pucFileBuffer);
+        lRetVal = sl_FsClose(pFileHandle, 0, 0, 0);
+        ERR_PRINT(lRetVal);
+        LOOP_FOREVER();
+    }
+
+    UART_PRINT("TFTP Read Successful \r\n");
+
+    // Close file (successfully written to FS)
+    lRetVal = sl_FsClose(pFileHandle, 0, 0, 0);
 }
 
 //*****************************************************************************

43af462c3e81cd86fa7cbb6bf8df03958d000911
Brandon To <brandon_to@hotmail.com>
2015-12-05 15:41:58 -0500

    Added boilerplate code for VC0706-based Serial Camera driver.

---
+++ Firmware/vc0706.c
@@ -0,0 +1,17 @@
+//*****************************************************************************
+//
+// vc0706.c
+//
+// Driver for the VC0706 Serial Camera Module.
+//
+// Created:
+// December 4, 2015
+//
+// Modified:
+// December 5, 2015
+//
+//*****************************************************************************
+
+#include "vc0706.h"
+
+// TODO (Brandon): Implement API for VC0706 driver
---
+++ Firmware/vc0706.h
@@ -0,0 +1,20 @@
+//*****************************************************************************
+//
+// vc0706.h
+//
+// Defines and Macros for the VC0706 Serial Camera Module.
+//
+// Created:
+// December 4, 2015
+//
+// Modified:
+// December 5, 2015
+//
+//*****************************************************************************
+
+#ifndef VC0706_H_
+#define VC0706_H_
+
+// TODO (Brandon): Design API for VC0706 driver
+
+#endif /* VC0706_H_ */

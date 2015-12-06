450c0c61f1d357e6ae835888e15f028d31700603
Brandon To <brandon_to@hotmail.com>
2015-12-05 20:03:04 -0500

    Added VC0706 interface file for abstracting away direct calls to VC0706 driver code.

---
+++ Firmware/vc0706_if.c
@@ -0,0 +1,20 @@
+//*****************************************************************************
+//
+// vc0706_if.c
+//
+// Implementation of API for using the VC0706 Serial Camera Module.
+//
+// Created:
+// December 5, 2015
+//
+// Modified:
+// December 5, 2015
+//
+//*****************************************************************************
+
+#include "vc0706_if.h"
+
+void CameraInit(unsigned short usSerialNum)
+{
+    // TODO (Brandon): Implement
+}
